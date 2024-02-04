be671e3e6d3d3b5a37f7b39d76503b87a49b5e7f
Aleksandr Davydov <113263675+Davydov2577@users.noreply.github.com>
2024-02-04 13:32:18 +0200

    task-3_task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = event.target.elements.email.value.trim();
+  const password = event.target.elements.password.value.trim();
 
+  if (!email || !password) {
+    alert("All form fields must be filled in");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    event.target.reset();
+  }
+});
